2d53e5d7dc0208be715cdbd5569938c4c9cfaf13
Jeremy Howard <github@jhoward.fastmail.fm>
2026-03-17 15:44:28 +1000

    bump

--- ipyai/__init__.py
+++ ipyai/__init__.py
@@ -1,4 +1,5 @@
-__version__ = "0.0.2"
+__version__ = "0.0.3"
+
 
 from .core import AI_LAST_PROMPT, AI_LAST_RESPONSE, DEFAULT_MODEL, IPyAIExtension, create_extension, load_ipython_extension, unload_ipython_extension
 
